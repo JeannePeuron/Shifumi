4893867243fe6675b44b318cb3633265e6cc6271
leika127 <97314006+leika127@users.noreply.github.com>
2025-07-25 10:43:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function getComputerChoice(){
 
-let Pierre = 0
+let pierre = 0
 let feuille = 1
 let ciseaux = 2
 
@@ -11,24 +11,76 @@ let convertValue = value * 3
 
 let finalValue = Math.floor(convertValue)
 
-    if(finalValue == Pierre){
-        return "Pierre"
+    if(finalValue == pierre){
+        return "pierre"
     } else if (finalValue == feuille){
-        return "Feuille"
+        return "feuille"
     } else if (finalValue == ciseaux){
-        return "Ciseaux"
+        return "ciseaux"
     }
 }
        
 
 
-console.log(getComputerChoice())
+//console.log(getComputerChoice())
 
 //Etape 3
 
 function getHumanChoice(){
     let message = prompt("SHI-FOU-MI")
     console.log(message)
+        return message
 }
 
-getHumanChoice()
+//getHumanChoice()
+
+//Etape 4
+
+let humanScore = 0
+let computerScore = 0
+
+//Etape 5
+
+function playRound(humanChoice, computerChoice){
+    
+        for(let choice = 0; choice < 10 ; choice++){
+
+            if (humanChoice === "ciseaux" && computerChoice === "feuille"){
+                return "ciseaux bat feuille!"
+            }
+            if (humanChoice === "ciseaux" && computerChoice === "pierre"){
+                return "pierre bat ciseaux!"
+            }
+            if (humanChoice === "ciseaux" && computerChoice === "ciseaux"){
+                return "match nul!"
+            }
+
+            if (humanChoice === "pierre" && computerChoice === "feuille"){
+                return "feuille bat pierre!"
+            }
+            if (humanChoice === "pierre" && computerChoice === "pierre"){
+                return "match nul!"
+            }
+            if (humanChoice === "pierre" && computerChoice === "ciseaux"){
+                return "pierre bat ciseaux!"
+            }
+   
+            if (humanChoice === "feuille" && computerChoice === "ciseaux"){
+                return "ciseaux bat feuille!"
+            }
+            if (humanChoice === "feuille" && computerChoice === "pierre"){
+                return "feuille bat pierre!"
+            }
+            if (humanChoice === "feuille" && computerChoice === "feuille"){
+                return "match nul!"
+            }
+        }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(playRound(humanSelection, computerSelection))
+
+
+
+
+    
